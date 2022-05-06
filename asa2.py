c954c0e0a457cd9d9b7eeec22035c85666527423
Alexey Aparnev <apich238@gmail.com>
2022-05-06 05:16:41 +0300

    drafted binary classification example. declared most of class interface.

--- asa2.py
+++ asa2.py
@@ -1,34 +1,81 @@
+import torch
+
 
 # draft of the API
-class Theory:
+class Theory(torch.nn.Module):
     def __init__(self):
         pass
 
-K=Theory()
+    def domain(self, name, dtype, shape: tuple):
+        pass
 
-# domains
-K.domain(name,dtype,shape)
-# vars
-K.variable(name,domain,precondition:predicate)
-# constants
-K.constant(name,domain,grounding)
-# functions
-K.function(name,domain_in,domain_out,grounding)
-# predicates
-K.predicate(name,domain_in,grounding)
-# axioms
-K.axiom(predicate_formula)
-# groundings
-## defined at declaration
-# learning
+    def variable(self, name, domain, precondition=None):
+        pass
 
-## optimizer
-opt=Adam(Theory.weights())
-## dataset
-dataldr=DataLoader()
-## objective function and loss function
-sat=K.SatAgg(var1=var1,var2=var2,...)
-loss=K.Loss(var1=var1,var2=var2,...)#1-K.SatAgg
-# quering
-K.evaluate_formula(formula,vars_values)
-# reasoning
+    def constant(self, name, domain, grounding):
+        pass
+
+    def function(self, name, domain_in, domain_out, grounding, trainable):
+        pass
+
+    def predicate(self, name, domain_in, grounding, trainable):
+        pass
+
+    def axiom(self, formula):
+        pass
+
+    def parameters(self, **kwargs):
+        pass
+
+    def forward(self, **vars):
+        SatAgg = 0
+        Loss = 1 - SatAgg
+        raise NotImplementedError("")
+        return SatAgg, Loss
+
+    def EvaluateFormula(self, formula, **vars):
+        pass
+
+
+def binary_classification_example():
+    K = Theory()
+
+    # domains
+    K.domain('point', 'fp32', (2,))
+    # vars
+    K.variable('x', 'point', )
+    K.variable('x_pos', 'point', precondition)
+    K.variable('x_neg', 'point', precondition)
+    # constants
+    # K.constant(name, domain, grounding)
+    # functions
+    # K.function(name, domain_in, domain_out, grounding)
+    # predicates
+    K.predicate('A', ['point'], grounding, True)
+    # axioms
+    K.axiom('FORALL(x_pos):A(x_pos)')
+    K.axiom('FORALL(x_neg):!A(x_neg)')
+
+    # optimizer
+    opt = torch.optim.Adam(K.parameters())
+    # dataset
+    ## generate data
+
+    ## dataset
+    from torch.utils.data import Dataset
+    train_dataset = None
+    test_dataset = None
+    ##dataloader
+
+    from torch.utils.data import DataLoader
+
+    train_loader = DataLoader(train_dataset)
+    test_loader = DataLoader(test_dataset)
+    ## objective function and loss function
+    sat, loss = K(x=[], x_pos=[], x_neg=[])
+    # quering
+    # 1.
+    K.evaluate_formula('A(x)', x=[])
+    # 2.
+    y = K.A(x=[])
+    # 3.
